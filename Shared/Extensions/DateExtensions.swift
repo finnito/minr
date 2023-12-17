bb5f9765097b242c330041041ada826ec6f3ce28
Finn Le Sueur <finn@lesueur.nz>
2023-12-17 13:50:22 +1300

    Feat: Adds endOfDay and currentTimestamp extensions

--- Shared/Extensions/DateExtensions.swift
+++ Shared/Extensions/DateExtensions.swift
@@ -9,10 +9,24 @@ import Foundation
 
 extension Date {
     var startOfDay: Date {
-        Calendar.current.startOfDay(for: self)
+        return Calendar.current.startOfDay(for: self)
+    }
+    
+    var endOfDay: Date {
+        let startOfDay = Calendar.current.startOfDay(for: self)
+        let components = DateComponents(hour: 23, minute: 59, second: 59)
+        if let date = Calendar.current.date(byAdding: components, to: startOfDay) {
+            return date
+        }
+        
+        return Date()
     }
     
     var isPastToday: Bool {
         return self < Date()
+    }
+    
+    static var currentTimeStamp: Int64{
+        return Int64(Date().timeIntervalSince1970 * 1000)
     }
 }
